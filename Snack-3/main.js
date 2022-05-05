3cd049233103f08f28fecdcd3efbd3668b43b3bf
Hirisci <www.alan92@icloud.com>
2022-05-05 14:53:45 +0200

    snack 3

--- Snack-3/main.js
+++ Snack-3/main.js
@@ -1,3 +1,28 @@
 // Crea due tag div con due id diversi:
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+
+const container = document.querySelector(".container");
+const divRed = document.createElement("div");
+divRed.classList.add("box")
+divRed.classList.add("box--red")
+const divGreen = document.createElement("div");
+divGreen.classList.add("box")
+divGreen.classList.add("box--green")
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+for (let i = 0; i < numbers.length; i++) {
+    const span = document.createElement("span")
+    if(numbers[i]%2===0){
+        span.innerText = numbers[i]
+        divGreen.append(span)
+    } else {
+        span.innerText = numbers[i]
+        divRed.append(span)
+    }    
+}
+
+container.append(divGreen);
+container.append(divRed)
