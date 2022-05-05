6ae9323b7ce914a81d7f3182d7abaee77a1bcfb0
Hirisci <www.alan92@icloud.com>
2022-05-05 12:23:17 +0200

    aggiunta consegna esercizio

--- Snack-1/main.js
+++ Snack-1/main.js
@@ -1,3 +1,6 @@
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
+//Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+
 const array=[];
 let somma=0;
 
